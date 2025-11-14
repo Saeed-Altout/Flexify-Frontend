148b17c3441540f1ea268907d1cf27d58073f852
Saeed-Altout <saeedaltoutpro@gmail.com>
2025-11-14 15:50:27 +0300

    Refactor project management components to enhance user experience by improving input validation in ProjectForm and consolidating project-related logic. Update localization for better support in English and Arabic, and streamline project submission processes to ensure all necessary features are included in the project schema.

---
+++ src/app/[locale]/dashboard/projects/_components/edit-project-modal.tsx
@@ -0,0 +1,25 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function EditProjectModal() {
+  return (
+    <Sheet>
+      <SheetTrigger>Edit</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
